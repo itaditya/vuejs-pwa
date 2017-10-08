a8bd5a5728a0b1838f53adffc7fcd578e2861932
itaditya <adityaa803@gmail.com>
2017-10-08 09:51:56 +0530

    https catapi bug fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,9 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyCNrKHVmponDlWTDbL04dpOMykMLrj9bzo"',
+  authDomain: '"cropchat-edc3f.firebaseapp.com"',
+  databaseURL: '"https://cropchat-edc3f.firebaseio.com"',
+  messagingSenderId: '"234877842454"',
+  projectId: '"cropchat-edc3f"',
+  storageBucket: '"cropchat-edc3f.appspot.com"'
 }
--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -22,13 +22,12 @@
 import parse from 'xml-parser'
 export default {
   mounted () {
-    this.$http.get('http://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+    this.$http.get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
       this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
     })
   },
   methods: {
     postCat () {
-      console.log(this.catUrl)
       this.$root.$firebaseRefs.cat.push(
         {
           'url': this.catUrl,
