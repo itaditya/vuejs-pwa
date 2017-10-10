194e22de665266739a5831f6185d8379b25c4167
itaditya <adityaa803@gmail.com>
2017-10-10 10:55:57 +0530

    share cat picture feature added

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--8-col">
-      <div class="picture">
-        <img :src="this.cat.url" />
+  <div>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--8-col">
+        <div class="picture">
+          <img :src="this.cat.url"/>
+        </div>
+        <div class="info">
+          <span>{{ this.cat.info }}</span>
+        </div>
       </div>
-      <div class="info">
-        <span>{{ this.cat.info }}</span>
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+        <div class="comment">
+          <span>{{ this.cat.comment }} {{ this.shareFeature}}</span>
+        </div>
+        <div class="actions">
+          <router-link class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" to="/post">ANSWER</router-link>
+        </div>
       </div>
     </div>
-    <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
-      <div class="comment">
-        <span>{{ this.cat.comment }}</span>
-      </div>
-      <div class="actions">
-        <router-link class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" to="/post">
-          ANSWER
-        </router-link>
-      </div>
+    <div class="share-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" @click.prevent="shareCat" v-if="shareFeature">
+      <i class="material-icons">share</i>
     </div>
   </div>
 </template>
@@ -26,23 +29,36 @@ export default {
   },
   methods: {
     getCat () {
-      console.log('a')
       if (navigator.onLine) {
-        console.log('b')
         return this.$root.cat.find(cat => cat['.key'] === this.$route.params.id)
       }
-      console.log('c')
       return JSON.parse(localStorage.getItem('cats')).find(cat => cat['.key'] === this.$route.params.id)
+    },
+    shareCat () {
+      if (this.shareFeature) {
+        navigator.share({
+          title: this.cat.info,
+          text: `${this.cat.comment}\n`,
+          url: this.cat.url
+        })
+      }
     }
   },
   data () {
     return {
-      cat: this.getCat()
+      cat: this.getCat(),
+      shareFeature: navigator.share !== undefined
     }
   }
 }
 </script>
 <style scoped>
+  .share-picture-button {
+    position: fixed;
+    right: 24px;
+    bottom: 24px;
+    z-index: 998;
+  }
   .picture > img {
     color: #fff;
     width:100%;
